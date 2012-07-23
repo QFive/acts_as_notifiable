1c776450ac3c4493ab42935b66e53d8aa8f98935
Damian Galarza <galarza.d@gmail.com>
2012-07-23 18:17:57 -0400

    Use composition instead of inheritance for base ActsAsNotifiable::Notification

--- lib/acts_as_notifiable/notification.rb
+++ lib/acts_as_notifiable/notification.rb
@@ -1,18 +1,31 @@
 module ActsAsNotifiable
-  class Notification < ActiveRecord::Base
-    include ActsAsNotifiable::DeliveryMethods
+  module Notification
+    extend ActiveSupport::Concern
 
-    before_create :notifiable_before_create
-    after_create :notifiable_after_create
+    included do
+      class_eval do
+        include ActsAsNotifiable::DeliveryMethods
+
+        before_create :notifiable_before_create
+        after_create :notifiable_after_create
+
+        scope :apn, where(:apns => true)
+        scope :unprocessed, where(:apn_processed => false)
+      end
+    end
 
     def notifiable_before_create
       notifiable_callback(notifiable, :before_create)
       notifiable_callback(self, :before_create)
+
+      true
     end
 
     def notifiable_after_create
       notifiable_callback(notifiable, :after_create)
       notifiable_callback(self, :after_create)
+
+      true
     end
 
     def notifiable_callback(obj, chain)
