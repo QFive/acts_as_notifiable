085faa63d1b74010e8b42a047f615b87bb44298c
Damian Galarza <galarza.d@gmail.com>
2012-08-15 09:57:06 -0400

    Notify only if a receiver is present to receive the notification

--- lib/acts_as_notifiable/notifiable/callback.rb
+++ lib/acts_as_notifiable/notifiable/callback.rb
@@ -1,11 +1,15 @@
 module ActsAsNotifiable
   module Notifiable
+    
+    # Callbacks for a notifiable object to deliver a notification
     module Callback
 
+      # Create a notification if there is a receiver to receive it
       def notification_callback
-        notify
+        notify if notice_receiver
       end
 
+      # Notify the receiver(s) of what happend
       def notify
         if enumerable_receiver?
           notice_receiver.each &method(:notify!)
@@ -21,6 +25,8 @@ module ActsAsNotifiable
                                   :target        => notice_target
       end
 
+      # Interpret a target which can be either a proc or a method
+      # evaluating it in the context of the instance of the notifable object
       def interpret_target(method)
         if method.is_a? Proc
           self.instance_eval(&method)
@@ -29,22 +35,27 @@ module ActsAsNotifiable
         end
       end
 
+      # Interpret the notice receiver
       def notice_receiver
         interpret_target(self.class.receiver)
       end
 
+      # Interpret the notice sender
       def notice_sender
         interpret_target(self.class.sender)
       end
 
+      # Interpret the notice target
       def notice_target
         interpret_target(self.class.target)
       end
 
+      # Determine whether or not a receiver is enumerable
       def enumerable_receiver?
         notice_receiver.is_a? Enumerable
       end
 
     end
+
   end
 end
