1da8b80d4571e4ad6790f8e0131bb5216622554b
Damian Galarza <galarza.d@gmail.com>
2012-08-16 20:26:52 -0400

    Adding documentation for BaseCourier

--- lib/acts_as_notifiable/couriers/base_courier.rb
+++ lib/acts_as_notifiable/couriers/base_courier.rb
@@ -2,11 +2,22 @@ module ActsAsNotifiable
   module Couriers
 
     # Base courier class for all couriers to inherit from
+    #
+    # A courier has 2 main abilities: 
+    #
+    # 1. Prepare a notification for delivery, allowing the courier to make modifications
+    #    to the notification object before it get's created by hooking into ActiveModel's
+    #    before_create callback.
+    #
+    # 2. Deliver the notification in some means. Delivery will be done after a notification
+    #    via ActiveModel's after_create callback
     # 
     # Provides a simple organization for delivering notifications
     class BaseCourier
 
       # Method to deliver a given notification with the courier
+      #
+      # This will instantiate a courier object and deliver the notification
       #
       # @param [Notification] notification Notification to deliver
       def self.deliver(notification)
@@ -14,6 +25,11 @@ module ActsAsNotifiable
         instance.deliver 
       end
 
+      # Method for preparing a given notification with the courier
+      #
+      # This will instantiate a courier object and call prepare
+      #
+      # @param [Notification] notification The notification to prepare
       def self.prepare(notification)
         instance = self.new(notification)
         instance.prepare
