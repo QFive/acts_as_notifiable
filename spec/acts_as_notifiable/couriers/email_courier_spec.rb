a76282b0e660247a3bd958fb4f18c47e21ddb6ce
Damian Galarza <galarza.d@gmail.com>
2012-09-21 18:13:16 -0400

    More robust specs for email courier

--- spec/acts_as_notifiable/couriers/email_courier_spec.rb
+++ spec/acts_as_notifiable/couriers/email_courier_spec.rb
@@ -1,15 +1,35 @@
 require 'spec_helper'
 
+class CustomNotification; end
+class CustomNotificationMailer; end
+
 describe ActsAsNotifiable::Couriers::EmailCourier do
-  it "should call a mailer for a notifiable" do
-    message = Message.new
-    notification = Notification.new(:notifiable => message)
+  let(:notification) { Notification.new(notifiable: Message.new) }
+  let(:courier) { ActsAsNotifiable::Couriers::EmailCourier.new(notification) }
 
-    courier = ActsAsNotifiable::Couriers::EmailCourier.new(notification)
-    courier.mailer.should == MessageNotificationMailer
+  describe "#deliver!" do
+    it "delivers an email" do
+      mail = double("Mail", deliver: nil)
+      MessageNotificationMailer.should_receive(:notification).and_return(mail)
+      mail.should_receive(:deliver)
+      courier.deliver!
+    end
   end
 
-  it "should call a mailer for a notification" do
-    pending
+  describe "#mailer" do
+    context "Notification instance" do
+      it "generates a mailer for the notifiable object" do
+        courier.mailer.should == MessageNotificationMailer
+      end
+    end
+
+    context "Custom notification class" do
+      let(:notification) { CustomNotification.new }
+
+      it "should call a mailer for a notification" do
+        courier.mailer.should == CustomNotificationMailer
+      end
+    end
   end
+
 end
