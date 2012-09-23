83db009532291ff2ac27a37b965fa9486a2c95c5
Damian Galarza <galarza.d@gmail.com>
2012-09-23 13:11:17 -0400

    rename spec

--- spec/acts_as_notifiable/notifiable_spec.rb
+++ spec/acts_as_notifiable/notifiable_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe ActsAsNotifiable::Notifiable do
 
-  it "should have many notifications" do
+  it "has many notifications" do
     message = Message.new
     message.should respond_to :notifications
   end
