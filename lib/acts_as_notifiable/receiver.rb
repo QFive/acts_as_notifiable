8e7ff79f43fbeea1094d1f48a99f54e6c649b32c
Damian Galarza <galarza.d@gmail.com>
2012-09-23 18:45:16 -0400

    More documentation behind can_receive_notification

--- lib/acts_as_notifiable/receiver.rb
+++ lib/acts_as_notifiable/receiver.rb
@@ -11,7 +11,18 @@ module ActsAsNotifiable
     end
 
     # Determine whether or not a receiver can receive a notification
-    # by the given type and the given courier method
+    # by the given type and the given courier method. By default a receiver will
+    # always be marked as able to receive a notification for a given courier.
+    #
+    # If you are looking to customize this behavior you must define the
+    # can_receive_[notification_type]_notification_[courier] method in your model
+    # which will allow control of whether or not to deliver the notification by
+    # a given courier.
+    #
+    # For example, if you have a message notification which has custom logic behind
+    # when you would want to send a notification via email to the receiver you could define
+    # a method named *can_receive_message_notification_email?* on the user which returns true
+    # whenever you would like to deliver an email notification.
     #
     # @param [Symbol] Type of notification to be delivered
     # @param [Symbol] Name of the courier being used to deliver the notification
